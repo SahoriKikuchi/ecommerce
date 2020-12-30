7f2ce8fe2fd6c204a0c2c540ba19b53f8df184b5
SahoriKikuchi <SahoriKikuchi@users.noreply.github.com>
2020-12-30 19:54:07 -0300

    criando o pageadmin e o tpl admin

--- index.php
+++ index.php
@@ -4,14 +4,23 @@ require_once("vendor/autoload.php"); //traz as dependencias do projeto
 
 use \Slim\Slim; //Namespaces
 use \Hcode\Page; //Namespaces
+use \Hcode\PageAdmin;
 
 $app = new Slim(); 
 
 $app->config('debug', true);
-
+// 1º Rota:
 $app->get('/', function() {
     
 	$page = new Page();
+
+	$page->setTpl("index"); //carrega o conteudo que ta dentro do template index
+
+});
+
+$app->get('/admin', function() {
+    
+	$page = new PageAdmin();
 
 	$page->setTpl("index"); //carrega o conteudo que ta dentro do template index
 
